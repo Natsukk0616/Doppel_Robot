a831012897e5111e78e315536006882aaa195348
Natsukk0616 < GitHub >
2026-09-14 15:49:35 +0800

    Upload Doppel Robot project files

---
+++ integration/esp32_comprehensive_test/servo_bus.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Arduino.h>
+
+void initServoBus();
+// 返回 true 表示该行是舵机命令；false 交给综合程序其他命令处理。
+bool handleServoCommand(char *line);
+void serviceServoSafety();
+bool servoSafetyTripped();
